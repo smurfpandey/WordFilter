4f20fdd3ef45a59dc1c55ce0b0ae4e0c74a60e6e
Neeraj Verma <neeraj.viman@gmail.com>
2018-11-01 22:36:33 +0530

    Initial commit

---
+++ WordFilter.Tests/WordFilterTest.cs
@@ -0,0 +1,53 @@
+﻿using WordFilterNS;
+using NUnit.Framework;
+
+namespace WordFilterTestNS
+{
+    [TestFixture]
+    class WordFilterTest
+    {
+
+        [Test]
+        public void ShouldDetectBadWordsInString()
+        {
+            WordFilter _wordFilter = new WordFilter();
+
+            Assert.IsTrue(_wordFilter.Blacklisted("this string contains the word skank"), "skank should be true");
+            Assert.IsTrue(_wordFilter.Blacklisted("this string contains the word SkAnK"), "SkAnK should be true");
+            Assert.IsTrue(_wordFilter.Blacklisted("this string contains the wordskank"), "wordskank should be true");
+            Assert.IsTrue(_wordFilter.Blacklisted("this string contains the skankword"), "skankword should be true");
+            Assert.IsFalse(_wordFilter.Blacklisted("this string is clean!"), "should be false");
+        }
+
+        [Test]
+        public void ShouldAddWordToBlacklist()
+        {
+            WordFilter _wordFilter = new WordFilter();
+            _wordFilter.AddWords(new string[] { "clean_word" });
+
+            Assert.IsTrue(_wordFilter.Blacklisted("this string is not clean with clean_word"), "clean_word should be true");
+            Assert.IsFalse(_wordFilter.Blacklisted("this string is clean"), "clean should be false");
+        }
+
+        [Test]
+        public void ShouldRemoveWordFromBlacklist()
+        {
+            WordFilter _wordFilter = new WordFilter();
+            _wordFilter.RemoveWord("skank");
+
+            Assert.IsFalse(_wordFilter.Blacklisted("this string contains the word skank"), "skank should be false as it has been removed");
+        }
+
+        [Test]
+        public void ShouldClearList()
+        {
+            WordFilter _wordFilter = new WordFilter();
+            _wordFilter.ClearList();
+
+            Assert.IsFalse(_wordFilter.Blacklisted("this string contains the word bitch"), "bitch should be false as the list is clear");
+
+            _wordFilter.AddWords(new string[] { "bitch" });
+            Assert.IsTrue(_wordFilter.Blacklisted("this string contains the word bitch"), "bitch should be true as it was readded");
+        }
+    }
+}
---
+++ WordFilter/WordFilter.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WordFilterNS
+{
+    public class WordFilter
+    {
+        private List<string> lstBadWords = new List<string>();
+
+        public WordFilter()
+        {
+            string badWords = File.ReadAllText("badwords.json");
+            lstBadWords = JsonConvert.DeserializeObject<List<string>>(badWords);
+        }
+
+        public bool Blacklisted(string sentence)
+        {
+            sentence = sentence.ToLower();
+            return lstBadWords.Exists(word => sentence.Contains(word));
+        }
+
+        public void AddWords(string[] arrWords)
+        {
+            lstBadWords.AddRange(arrWords);
+        }
+
+        public void RemoveWord(string word)
+        {
+            lstBadWords.Remove(word);
+        }
+
+        public void ClearList()
+        {
+            lstBadWords = new List<string>();
+        }
+    }
+}
